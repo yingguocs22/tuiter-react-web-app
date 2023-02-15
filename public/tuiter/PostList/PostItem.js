28404eccb30cbdce3cb92624917a40b0c7408c05
yingguocs22 <guoyingcs@163.com>
2023-02-15 13:41:07 -0800

    update home screen

--- public/tuiter/PostList/PostItem.js
+++ public/tuiter/PostList/PostItem.js
@@ -12,16 +12,16 @@ const hasContent = (post) => {
                 <img class="wd-content-image" src=${post.image}/>
             </div>
 
-            <div class="wd-second-content">
+            <div class="wd-second-content wd-font-size-15">
                 <div class="text-truncate">
                     ${post.context_title}
                 </div>
 
-                <div class="wd-font-size-15 wd-second-content-text">
+                <div class="wd-fg-color-light-gray">
                     ${post.context_text}
                 </div>
                 
-                <div class="wd-font-size-15 wd-second-content-text">
+                <div class="wd-fg-color-light-gray">
                     ${hasLink(post)}
                 </div>
             </div>     
@@ -41,36 +41,36 @@ const hasLink = (post) => {
 const PostItem = (post) => {
     return(`
                 <li class="list-group-item">          
-                    <div class="row">
-                        <div class="col-1 text-center">
+                    <div class="float-end">
+                        <div class="wd-float-left-10">
                             <img class="wd-avatar" src=${post.avatar}/>
                         </div>
     
-                        <div class="col-11">
+                        <div class="wd-float-left-90">
                             <div class="d-flex justify-content-between">
                                 <div>
                                     <span>${post.userName}</span> 
                                     <i class="fas fa-check-circle ms-1"></i>
-                                    <span>@${post.handle}  • ${post.time}</span>                             
+                                    <span class="wd-fg-color-light-gray">@${post.handle}  • ${post.time}</span>                             
                                 </div>
                                 <div>
                                     <i class="fas fa-ellipsis-h text-dark"></i>                      
                                 </div>
                             </div>
                 
-                            <div class="mb-2">
+                            <div class="mb-2 wd-font-size-15 wd-fg-color-white-smoke">
                                 ${post.title}
                             </div>
                             
                             ${hasContent(post)}
                             
-                            <div class="wd-icon-container d-flex justify-content-between align-items-center">
+                            <div class="wd-icon-container d-flex justify-content-between align-items-center wd-font-size-15">
                                 <a href="#chat"><i class="far fa-comment"></i><span>${post.comment}</span></a>
                                 <a href="#reply"><i class="fa-solid fa-repeat"></i><span>${post.repeat}</span></a>
                                 <a href="#like"><i class="fa-regular fa-heart"></i><span>${post.like}</span></a>
                                 <a href="#upload"><i class="fa-solid fa-arrow-up-from-bracket"></i></i><span>${post.share}</span></a>
                             </div>
-                        </div>     
+                        </div>  
                     </div>
                 </li>
                
