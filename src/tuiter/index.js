9d6c660f6d01316dc33232b58b9d76e3ecab8f25
yingguocs22 <guoyingcs@163.com>
2023-02-25 21:04:44 -0800

    three part

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,13 +1,37 @@
 import {Link}
     from "react-router-dom";
 import Nav from "../nav";
+import NavigationSidebar from "./navigation-sidebar";
+import WhoToFollowListItem
+    from "./who-to-follow-list/who-to-follow-list-item";
+import WhoToFollowList from "./who-to-follow-list";
+import PostSummaryList from "./post-summary-list";
 
 function Tuiter() {
     return(
         <div>
             <Nav/>
-            <h1>Tuiter</h1>
+            {/*<NavigationSidebar active="home"/>*/}
+            {/*<WhoToFollowList/>*/}
+            {/*<h1>Tuiter</h1>*/}
+
+            <div className="row mt-2">
+                <div className="col-2 col-md-2 col-lg-1 col-xl-2">
+                    <NavigationSidebar active="explore"/>
+                </div>
+                <div className="col-10 col-md-10 col-lg-7 col-xl-6"
+                     style={{"position": "relative"}}>
+                    <PostSummaryList/>
+                </div>
+                <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+                    <WhoToFollowList/>
+                </div>
+            </div>
+
+
         </div>
+
+
     )
 }
 export default Tuiter
