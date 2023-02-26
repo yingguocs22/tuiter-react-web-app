a6b9200ae388ad133266089373bfed78b854d0bb
yingguocs22 <guoyingcs@163.com>
2023-02-25 16:08:56 -0800

    a7 update root

--- src/App.js
+++ src/App.js
@@ -1,47 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+//
+// function App() {
+//   const a = 123;
+//   const b = 234;
+//   let c = a + b;
+//   const d = [a, b, c];
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           {/*Edit <code>src/App.js</code> and save to reload.*/}
+//           Welcome to Web Dev CS5610 Section 6
+//         </p>
+//         <p>
+//           a = {a}
+//           <br/>
+//           b = {b}
+//           <br/>
+//           c = {c}
+//           <br/>
+//           d = {d}
+//           <ul>
+//             <li>{d[0]}</li>
+//             <li>{d[1]}</li>
+//             <li>{d[2]}</li>
+//           </ul>
+//           <ol>
+//             {d.map(item => <li>{item}</li>)}
+//           </ol>
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+//
+// export default App;
 
 function App() {
-  const a = 123;
-  const b = 234;
-  let c = a + b;
-  const d = [a, b, c];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {/*Edit <code>src/App.js</code> and save to reload.*/}
-          Welcome to Web Dev CS5610 Section 6
-        </p>
-        <p>
-          a = {a}
-          <br/>
-          b = {b}
-          <br/>
-          c = {c}
-          <br/>
-          d = {d}
-          <ul>
-            <li>{d[0]}</li>
-            <li>{d[1]}</li>
-            <li>{d[2]}</li>
-          </ul>
-          <ol>
-            {d.map(item => <li>{item}</li>)}
-          </ol>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Hello World!</h1>
   );
 }
-
 export default App;
