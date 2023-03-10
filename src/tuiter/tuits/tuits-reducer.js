d164f8b9c224177c4d28bdea87f0a4dd6a6969ff
yingguocs22 <guoyingcs@163.com>
2023-03-10 08:01:14 +0800

    add 3.1 & 3.2

---
+++ src/tuiter/tuits/tuits-reducer.js
@@ -0,0 +1,7 @@
+import { createSlice } from "@reduxjs/toolkit";
+import tuits from './tuits.json';
+
+const tuitsSlice = createSlice({ name: 'tuits',
+    initialState: tuits
+});
+export default tuitsSlice.reducer;
