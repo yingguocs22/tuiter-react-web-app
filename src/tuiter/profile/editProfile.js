5265b66599ea784572287b99bdf8bf1985446f1d
yingguocs22 <guoyingcs@163.com>
2023-03-26 12:44:05 -0700

    update profile

--- src/tuiter/profile/editProfile.js
+++ src/tuiter/profile/editProfile.js
@@ -1,18 +1,43 @@
 import React, {useState} from "react";
 import "./index.css";
 import {useDispatch} from "react-redux";
-import {updateProfile} from "./profile-reducer";
+import {updateProfile, deleteProfile} from "./profile-reducer";
+import {useNavigate} from "react-router";
 
 const EditProfile = () => {
+    // save and nagivate to profile
+    const navigate = useNavigate();
+    const navigateProfile = () => {
+        navigate('../profile');
+    };
+
     let [userName, setUserName] = useState('');
+    let [userBio, setUserBio] = useState('');
+    let [userLocation, setUserLocation] = useState('');
+    let [userWebsite, setUserWebsite] = useState('');
+    let [userBirthDate, setUserBirthDate] = useState('');
     const dispatch = useDispatch();
+    const deleteOldProfile = () => {
+        console.log("delete")
+        dispatch(deleteProfile());
+    }
+
     const saveClickHandler = () => {
-        const newName = {
-            "firstName": userName,
-            "followersCount": 111,
-        }
-        dispatch(updateProfile(newName));
         console.log(userName);
+        console.log(userBio);
+        console.log(userLocation);
+        console.log(userWebsite);
+        const newProfile = {
+            name: userName,
+            bio: userBio,
+            location: userLocation,
+            website:userWebsite,
+            dateOfBirth: userBirthDate
+        }
+        dispatch(updateProfile(newProfile));
+
+        deleteOldProfile();
+        navigateProfile();
     }
 
     return(
@@ -20,7 +45,7 @@ const EditProfile = () => {
             <li className="list-group-item">
                 <div className="row">
                     <div className='col-1'>
-                        <i className="bi bi-x"></i>
+                        <i onClick={navigateProfile} className="bi bi-x"></i>
                     </div>
                     <div className='col-10 float-start'>
                         <span className="wd-title wd-margin-left-0">Edit profile</span>
@@ -46,7 +71,39 @@ const EditProfile = () => {
                           onChange={(event) => setUserName(event.target.value)}>
                 </textarea>
 
+                <label className="wd-appendfix">Bio:</label><br/>
+                <textarea value={userBio}
+                          placeholder="About yourself"
+                          className="textarea-full"
+                          onChange={(event) => setUserBio(event.target.value)}>
+                </textarea>
 
+                <label className="wd-appendfix">Location:</label><br/>
+                <textarea value={userLocation}
+                          placeholder="Your location"
+                          className="textarea-full"
+                          onChange={(event) => setUserLocation(event.target.value)}>
+                </textarea>
+
+                <label className="wd-appendfix" for="input-website">Website:</label><br/>
+                <input type="website"
+                       value={userWebsite}
+                       id="input-website"
+                       placeholder="Your website"
+                       className="textarea-full"
+                       onChange={(event) => setUserWebsite(event.target.value)}>
+                </input>
+
+                <label className="wd-appendfix" for="text-fields-dob">Birth Date:</label><br/>
+                <input type="date"
+                       value={userBirthDate}
+                       id="text-fields-dob"
+                       onChange={(event) => setUserBirthDate(event.target.value)}/><br/><br/>
+
+                <div className='row'>
+                    <span className='col-11'>Switch to professional</span>
+                    <i className="bi bi-arrow-right col-1"></i>
+                </div>
             </li>
         </ul>
     );
--- src/tuiter/profile/profile-reducer.js
+++ src/tuiter/profile/profile-reducer.js
@@ -1,6 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 import infos from './myInfo.json';
 
+const templatePro = {
+    "handle": "@yingguo",
+    "profilePicture": "ying.jpg",
+    "bannerPicture": "background.jpg",
+    "dateJoined": "9/2021",
+    "followingCount": 340,
+    "followersCount": 223
+}
+
 const profileSlice = createSlice({
     name: 'profile',
     initialState: infos,
@@ -8,9 +17,13 @@ const profileSlice = createSlice({
         updateProfile(state, action) {
             state.unshift({
                 ...action.payload,
+                ...templatePro,
             })
         },
+        deleteProfile(state) {
+            state.splice(1, 1)
+        }
     }
 });
-export const {updateProfile} = profileSlice.actions;
+export const {updateProfile, deleteProfile} = profileSlice.actions;
 export default profileSlice.reducer;
