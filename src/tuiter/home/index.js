05b9993df4e5e3af221d691ed0a9e363e19af643
yingguocs22 <guoyingcs@163.com>
2023-03-31 17:51:23 -0700

    update name

--- src/tuiter/home/index.js
+++ src/tuiter/home/index.js
@@ -17,13 +17,13 @@ function HomeComponent() {
                 {/*<div className="col-2 col-md-2 col-lg-1 col-xl-2">*/}
                 {/*    <NavigationSidebar active="explore"/>*/}
                 {/*</div>*/}
-                <div className="col-10 col-md-10 col-lg-8 col-xl-7"
+                <div className="col-10 col-md-10 col-lg-9 col-xl-8"
                      style={{"position": "relative"}}>
                     <h4>Home</h4>
                     <WhatsHappening/>
                     <TuitsList/>
                 </div>
-                <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-5">
+                <div className="d-sm-none d-md-none d-lg-block col-lg-3 col-xl-4">
                     {/*<PostSummaryList/>*/}
                     <WhoToFollowList/>
                 </div>
