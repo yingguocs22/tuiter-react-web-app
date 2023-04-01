a37fbb111cc736c7da59a105a76b907ca436b6cd
yingguocs22 <guoyingcs@163.com>
2023-03-31 17:25:01 -0700

    update log

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 const API_BASE = process.env.REACT_APP_API_BASE;
 console.log(API_BASE)
 const TUITS_API = `${API_BASE}/tuits`;
-console.log('good')
 
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
