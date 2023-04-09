ae49750256b8e2d71cbd49f6d45c8adb1498b986
yingguocs22 <guoyingcs@163.com>
2023-04-08 22:29:52 -0700

    test for render

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
-const TUITS_API = 'http://localhost:4000/api/tuits';
-// const TUITS_API = 'https://tuiter-node-server-app-a9-ts3g.onrender.com/api/tuits';
+// const TUITS_API = 'http://localhost:4000/api/tuits';
+const TUITS_API = 'https://tuiter-node-server-app-a9-ts3g.onrender.com/api/tuits';
 // const TUITS_API = 'https://tuiter-node-server-app-mha8.onrender.com/api/tuits';
 // const API_BASE = process.env.REACT_APP_API_BASE;
-// console.log("start")
+// console.log("start ")
 // console.log(API_BASE)
 // const TUITS_API = `${API_BASE}/tuits`;
 
